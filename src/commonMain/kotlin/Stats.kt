6ca8137dc9aaa1bd8f324b3a28bfa71d65ee6209
hfhbd <hfhbd@users.noreply.github.com>
2021-06-03 22:32:10 +0200

    Use lazy whisker calculation

--- src/commonMain/kotlin/Stats.kt
+++ src/commonMain/kotlin/Stats.kt
@@ -1,48 +1,63 @@
+
 import kotlin.math.*
 
-interface Stats {
-    val min: Double
-    val max: Double
-    val median: Double
-    val average: Double
+data class Stats(
+    val count: Int,
+    val min: Double,
+    val max: Double,
+    val median: Double,
+    val average: Double,
 
-    val lowerQuantile: Double
-    val upperQuantile: Double
-    val iqr: Double
+    val std: Double,
+    val variance: Double,
 
-    val whiskerLength: Double
-    val lowerWhisker: Double
-    val upperWhisker: Double
-    val lowerExtrema: List<Double>
-    val upperExtrema: List<Double>
+    val lowerQuantile: Double,
+    val upperQuantile: Double,
+
+    val whisker: Lazy<Whisker>
+) {
+
+    data class Whisker(
+        val iqr: Double,
+        val whiskerLength: Double,
+        val lowerWhisker: Double,
+        val upperWhisker: Double,
+        val lowerExtrema: List<Double>,
+        val upperExtrema: List<Double>
+    )
 
     companion object {
-        operator fun <T : Comparable<T>> invoke(
-            values: List<T>,
+        operator fun <T> invoke(
+            values: Iterable<T>,
             whiskerLength: Double = 1.5,
             selector: (T) -> Double
-        ): Stats {
-            require(values.isNotEmpty())
-            val sortedValues = values.map(selector).sorted()
-            return object : Stats {
-                override val min: Double = sortedValues.minOrNull()!!
-                override val max: Double = sortedValues.maxOrNull()!!
-                override val median: Double = sortedValues[sortedValues.size / 2]
-                override val average: Double = sortedValues.average()
+        ) = invoke(sortedValues = values.map(selector).sorted(), whiskerLength = whiskerLength)
 
-                override val lowerQuantile: Double = sortedValues[(sortedValues.size * 0.25).toInt()]
-                override val upperQuantile: Double = sortedValues[(sortedValues.size * 0.75).toInt()]
-                override val iqr = upperQuantile - lowerQuantile
+        operator fun invoke(sortedValues: List<Double>, whiskerLength: Double = 1.5): Stats {
+            val count = sortedValues.size
+            val min: Double = sortedValues.minOrNull()!!
+            val max: Double = sortedValues.maxOrNull()!!
+            val median: Double = sortedValues[sortedValues.size / 2]
+            val average: Double = sortedValues.average()
 
-                override val whiskerLength = whiskerLength
-                override val lowerWhisker = (lowerQuantile - whiskerLength * iqr).let {
+
+            val variance = sortedValues.sumOf { (it - average).pow(2) } / count
+            val std = sqrt(variance)
+
+            val lowerQuantile: Double = sortedValues[(sortedValues.size * 0.25).toInt()]
+            val upperQuantile: Double = sortedValues[(sortedValues.size * 0.75).toInt()]
+
+            val whisker = lazy {
+                val iqr = upperQuantile - lowerQuantile
+
+                val lowerWhisker = (lowerQuantile - whiskerLength * iqr).let {
                     if (min >= it) {
                         max(min, it)
                     } else {
                         sortedValues.takeLastWhile { v -> v > it }.first()
                     }
                 }
-                override val upperWhisker = (upperQuantile + whiskerLength * iqr).let {
+                val upperWhisker = (upperQuantile + whiskerLength * iqr).let {
                     if (max <= it) {
                         min(max, it)
                     } else {
@@ -50,11 +65,32 @@ interface Stats {
                     }
                 }
 
-                override val lowerExtrema: List<Double> = sortedValues.takeWhile { it < lowerWhisker }
-                override val upperExtrema: List<Double> =
+                val lowerExtrema: List<Double> = sortedValues.takeWhile { it < lowerWhisker }
+                val upperExtrema: List<Double> =
                     sortedValues.takeLastWhile { it > upperWhisker }
 
+                Whisker(
+                    iqr = iqr,
+                    whiskerLength = whiskerLength,
+                    lowerWhisker = lowerWhisker,
+                    upperWhisker = upperWhisker,
+                    lowerExtrema = lowerExtrema,
+                    upperExtrema = upperExtrema
+                )
             }
+
+
+            return Stats(
+                count = count,
+                min = min, max = max,
+                median = median,
+                average = average,
+                variance = variance,
+                std = std,
+                lowerQuantile = lowerQuantile,
+                upperQuantile = upperQuantile,
+                whisker = whisker
+            )
         }
     }
 }
